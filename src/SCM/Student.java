d5d2d1649a73c414428cf2d5f3110478ffb70bb1
unknown <nghiaquyen.2801@gmail.com>
2023-05-11 08:37:57 +0700

    Student

--- src/SCM/Student.java
+++ src/SCM/Student.java
@@ -1,9 +1,63 @@
-package SCM;
-
-public class Student extends StudentGrade {
+package System;
+import java.util.Scanner;
+//this class contains information of a student including student ID, name, score, address, phone number, email,.
+public class Student extends StudentGrade{
 	private String name;
     private int age;
     private String studentId;
     private String major;
     private double gpa;
+    // add constructor, get, set , input and print student information
+    public Student(String name, int age, String studentId, String major, double gpa) {
+		this.name = name;
+		this.age = age;
+		this.studentId = studentId;
+		this.major = major;
+		this.gpa = gpa;
+	}
+    public Student() {
+		// TODO Auto-generated constructor stub
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public String getStudentId() {
+		return studentId;
+	}
+	public void setStudentId(String studentId) {
+		this.studentId = studentId;
+	}
+	public String getMajor() {
+		return major;
+	}
+	public void setMajor(String major) {
+		this.major = major;
+	}
+	public double getGpa() {
+		return gpa;
+	}
+	public void setGpa(double gpa) {
+		this.gpa = gpa;
+	}
+	Scanner kb=new Scanner(System.in);
+	void input() {
+		System.out.print("Enter Name:");name=kb.nextLine();
+		System.out.print("Enter Age:");age=kb.nextInt();
+		System.out.print("Enter StudentId:");studentId=kb.nextLine();
+		System.out.print("Enter Major:");major=kb.nextLine();
+		System.out.print("Enter Gpa:");gpa=kb.nextDouble();
+	}
+    void output() {
+    	System.out.println("Name:"+name+"\nAge:"+age+"\nStudentId:"+studentId+"\nMajor:"+major+"\nGpa:"+gpa);
+    }
+    
 }
