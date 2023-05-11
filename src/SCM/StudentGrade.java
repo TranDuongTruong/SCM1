40e038fcd2e494b45cf55c96461363efbe24ca0e
Long Phan <hloeng.business@gmail.com>
2023-05-11 08:40:28 +0700

    StudentGrade

--- src/SCM/StudentGrade.java
+++ src/SCM/StudentGrade.java
@@ -1,9 +1,61 @@
-package SCM;
-
+package System;
+//this class manage students' scores 
+//with specific subjects such as math, Literature, English, Physics.
 public class StudentGrade {
+	
 	private float  mathGrade;
 	private float  literatureGrade;
     private float  englishGrade;
     private float  physicsGrade;
     private float  mediumGrade;
+    
+    
+	public float  getMediumGrade() {
+		mediumGrade=mathGrade+literatureGrade+englishGrade+physicsGrade)/4;
+		return mediumGrade;
+	}
+
+	public void setMediumGrade(int mediumGrade) {
+		this.mediumGrade = mediumGrade;
+	}
+
+	public float  getMathGrade() {
+		
+		return mathGrade;
+	}
+
+	public void setMathGrade(int mathGrade) {
+		this.mathGrade = mathGrade;
+	}
+
+	public float  getLiteratureGrade() {
+		
+		return literatureGrade;
+	}
+
+	public void setLiteratureGrade(int literatureGrade) {
+		this.literatureGrade = literatureGrade;
+	}
+
+	public float  getEnglishGrade() {
+		
+		return englishGrade;
+	}
+
+	public void setEnglishGrade(int englishGrade) {
+		this.englishGrade = englishGrade;
+	}
+
+	public float  getPhysicsGrade() {
+		
+		return physicsGrade;
+	}
+
+	public void setPhysicsGrade(int physicsGrade) {
+		this.physicsGrade = physicsGrade;
+	}
+	public float AverageScore(){
+	    return (physicsGrade+englishGrade+literatureGrade+mathGrade)/4;
+	}
+
 }
